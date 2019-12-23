e74283e3808d441a007e963bbb31fcb43563fc05
aleam0722 <cxb444555@outlook.com>
2019-12-23 22:00:11 +0800

    创建编程入口SparkContext

--- spark-parent1902/core/src/main/scala/WordCountInScala.scala
+++ spark-parent1902/core/src/main/scala/WordCountInScala.scala
@@ -1,5 +1,10 @@
-class WordCountInScala {
-  def main(args: Array[String]): Unit = {
+import org.apache.spark.{SparkConf, SparkContext}
 
+object WordCountInScala {
+  def main(args: Array[String]): Unit = {
+    /*创建编程入口SparkContext*/
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCountInScala")
+    val sparkContext = new SparkContext(sparkConf)
+    println(sparkContext)
   }
 }
