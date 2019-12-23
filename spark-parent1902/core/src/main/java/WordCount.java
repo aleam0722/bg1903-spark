61afd85e4fc3c1b555b1a630fa4fcaac385f689e
aleam0722 <cxb444555@outlook.com>
2019-12-23 21:12:34 +0800

    打印JavaSparkContext对象测试提示错误org.apache.spark.SparkException: A master URL must be set in your configuration

--- spark-parent1902/core/src/main/java/WordCount.java
+++ spark-parent1902/core/src/main/java/WordCount.java
@@ -3,9 +3,12 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class WordCount {
     public static void main (String[] args){
-        /*观察JavaSparkContext源码发现，创建JavaSparkContext对象需要传传入SparkConf对象，因此需要创建SparkConf*/
+
+        /*观察JvaSparkContext源码发现，创建JavaSparkContext对象需要传传入SparkConf对象，因此需要创建SparkConf*/
         SparkConf sc = new SparkConf();
         /*创建spark程序入口JavaSparkContext的对象*/
         JavaSparkContext jsc = new JavaSparkContext();
+
+        System.out.println(jsc);
     }
 }
