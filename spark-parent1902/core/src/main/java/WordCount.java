f782d79435dac2e1ee17bc153cd6e79e16f5096e
aleam0722 <cxb444555@outlook.com>
2019-12-23 21:05:44 +0800

    创建spark程序入口JavaSparkContext的对象

---
+++ spark-parent1902/core/src/main/java/WordCount.java
@@ -0,0 +1,9 @@
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class WordCount {
+    public static void main (String[] args){
+
+        /*创建spark程序入口JavaSparkContext的对象*/
+        JavaSparkContext jsc = new JavaSparkContext();
+    }
+}
