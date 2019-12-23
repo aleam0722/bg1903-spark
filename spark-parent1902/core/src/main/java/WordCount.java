3dd779cb9cd8bc4446c277147cff99d6fbca6a6f
aleam0722 <cxb444555@outlook.com>
2019-12-23 21:08:56 +0800

    创建spark程序入口JavaSparkContext的对象

--- spark-parent1902/core/src/main/java/WordCount.java
+++ spark-parent1902/core/src/main/java/WordCount.java
@@ -1,8 +1,10 @@
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class WordCount {
     public static void main (String[] args){
-
+        /*观察JavaSparkContext源码发现，创建JavaSparkContext对象需要传传入SparkConf对象，因此需要创建SparkConf*/
+        SparkConf sc = new SparkConf();
         /*创建spark程序入口JavaSparkContext的对象*/
         JavaSparkContext jsc = new JavaSparkContext();
     }
