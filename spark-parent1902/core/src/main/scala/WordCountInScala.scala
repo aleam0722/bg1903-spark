791ab4c24cff5dd829883a847f8ac3ccda5ec5f7
aleam0722 <cxb444555@outlook.com>
2019-12-23 22:06:26 +0800

    获取数据并进行操作

--- spark-parent1902/core/src/main/scala/WordCountInScala.scala
+++ spark-parent1902/core/src/main/scala/WordCountInScala.scala
@@ -5,6 +5,13 @@ object WordCountInScala {
     /*创建编程入口SparkContext*/
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCountInScala")
     val sparkContext = new SparkContext(sparkConf)
-    println(sparkContext)
+
+
+    /*获取数据并操作*/
+    val lines = sparkContext.textFile("E:/word.txt")
+    val words = lines.flatMap(_.split(","))
+    val kv = words.map(word => (word, 1))
+    val result = kv.reduceByKey(_ + _)
+    result.foreach(println)
   }
 }
