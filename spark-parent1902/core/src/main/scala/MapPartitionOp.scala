c6f787a4efd613a17420c753165f77a227710d6e
aleam0722 <cxb444555@outlook.com>
2019-12-25 20:46:28 +0800

    mappartition就是根据分区，对分区内的数据调用map，慎用，因为会一次加载所有的数据

---
+++ spark-parent1902/core/src/main/scala/MapPartitionOp.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object MapPartitionOp {
+  val sc = new SparkContext(new SparkConf().setMaster("local[*]").setAppName("MapPartitionOp"))
+  def main(args: Array[String]): Unit = {
+    mapPatitinOp(sc)
+
+  }
+  def mapPatitinOp(sc: SparkContext): Unit = {
+    val lis = 1 to 20
+    val lisDRR = sc.parallelize(lis)
+
+    val ret = lisDRR.mapPartitions(partition =>{
+      partition.map(num => {
+        (num,1)
+      })
+    })
+//    ret.foreach(println)
+  }
+}
