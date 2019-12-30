2efb3cd73fffb56dbf69a40d8f4b9359b849edfa
aleam0722 <cxb444555@outlook.com>
2019-12-30 19:46:44 +0800

    同过sparkSession加载一个json格式的数据并打印出schema信息

--- spark-parent1902/spark_sql/src/main/scala/SparkSqlOp.scala
+++ spark-parent1902/spark_sql/src/main/scala/SparkSqlOp.scala
@@ -2,9 +2,30 @@ import org.apache.spark.sql.SparkSession
 
 object SparkSqlOp {
   /*sparkSession的构建*/
-  val spark = SparkSession.builder()
+  val spark = SparkSession.builder().master("local[*]").appName("SparkSqlOp").getOrCreate()
   def main(args: Array[String]): Unit = {
+    loadData(spark)
+  }
+  def loadData(spark:SparkSession):Unit = {
+    /*加载一个json格式的数据*/
+    val dataFrame = spark.read.json("File:///E:/sparkData/sql/account.json")
+    dataFrame.printSchema()
 
+    /**
+     * result-------->
+     * root
+     * |-- account_number: long (nullable = true)
+     * |-- address: string (nullable = true)
+     * |-- age: long (nullable = true)
+     * |-- balance: long (nullable = true)
+     * |-- city: string (nullable = true)
+     * |-- email: string (nullable = true)
+     * |-- employer: string (nullable = true)
+     * |-- firstname: string (nullable = true)
+     * |-- gender: string (nullable = true)
+     * |-- lastname: string (nullable = true)
+     * |-- state: string (nullable = true)
+     */
   }
 
 }
