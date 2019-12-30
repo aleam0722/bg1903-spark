a4a118e2096bb2ceee9e9ca10dd47b1ef21345f2
aleam0722 <cxb444555@outlook.com>
2019-12-30 19:56:29 +0800

    show方法显示数据，printlnSchema方法打印schema

--- spark-parent1902/core/src/main/scala/day3/PartitionTest.scala
+++ spark-parent1902/core/src/main/scala/day3/PartitionTest.scala
@@ -2,7 +2,9 @@ package day3
 
 import java.net.URL
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{Partitioner, SparkConf, SparkContext}
+
+import scala.collection.mutable
 /**
  * 自定分区
  * 数据中有不同的学科,将输出的一个学科生成一个文件
@@ -13,23 +15,23 @@ object PartitionTest {
   def main(args: Array[String]): Unit = {
     partitionTest(sc)
   }
-/*去除URL*/
+  /*去除URL*/
   def partitionTest(sd: SparkContext):Unit ={
     val lines = sc.textFile("File:///e:/access.txt")
     val takeURL = lines.map{case (line) =>
-        val URL = line.split("\\s+")(1)
+      val URL = line.split("\\s+")(1)
       (URL,1)
     }
     /*获取各个URL的点击数*/
     val URLCount = takeURL.reduceByKey(_+_)
     /*根据URl中的关键字转换为学科字符串串--->http://   ${java}   .learn.com/java/javaee.shtml*/
     val subjectAndCount = URLCount.map{case (urlStr,count) =>
-        val subject = new URL(urlStr).getHost
+      val subject = new URL(urlStr).getHost
       (subject,count)
     }
     /*因为是根据学科进行分区，而分区分区存放在RDD--->subjectAndCount的key中，所以我们需要收集所有的key值*/
     val subjects = subjectAndCount.keys.distinct.collect
-    /*创建一个自定义分区器，传入我们分区依据的字段，也就是我们的学科subject字段*/
+    /*创建一个自定义分区器对象，传入我们分区依据的字段，也就是我们的学科subject字段*/
     val partitioner = new SubjectPartitioner(subjects)
     /*进行分区*/
     val result = subjectAndCount.partitionBy(partitioner)
@@ -38,5 +40,19 @@ object PartitionTest {
     subjectAndCount.foreach(println)
   }
 
+}
 
+/*创建我们的自定义分器*/
+class SubjectPartitioner(subject: Array[String])  extends Partitioner{
+  /*创建一个分区用于存放学科和分区号*/
+  val subjects = new mutable.HashMap[String,Int]()
+  /*设置一个计数器用于生成分区号*/
+  var i = 0
+  for (s<-subject){
+    subjects += (s -> i)
+    i+=1 //分区自增
+  }
+  override def numPartitions: Int = subject.size
+
+  override def getPartition(key: Any): Int = subjects.getOrElse(key.toString,0)
 }
--- spark-parent1902/spark_sql/src/main/scala/SparkSqlOp.scala
+++ spark-parent1902/spark_sql/src/main/scala/SparkSqlOp.scala
@@ -4,12 +4,13 @@ object SparkSqlOp {
   /*sparkSession的构建*/
   val spark = SparkSession.builder().master("local[*]").appName("SparkSqlOp").getOrCreate()
   def main(args: Array[String]): Unit = {
-    loadData(spark)
+//    loadData(spark)
+    showData(spark)
   }
   def loadData(spark:SparkSession):Unit = {
     /*加载一个json格式的数据*/
     val dataFrame = spark.read.json("File:///E:/sparkData/sql/account.json")
-    dataFrame.printSchema()
+//    dataFrame.printSchema()
 
     /**
      * result-------->
@@ -26,6 +27,41 @@ object SparkSqlOp {
      * |-- lastname: string (nullable = true)
      * |-- state: string (nullable = true)
      */
+    spark.stop()
   }
+
+  def showData(spark: SparkSession): Unit = {
+    val dataFrame = spark.read.json("File:///E:/sparkData/sql/account.json")
+    dataFrame.show()
+    spark.stop()
+
+    /**
+     * +--------------+--------------------+---+-------+-----------+--------------------+---------+---------+------+---------+-----+
+     * |account_number|             address|age|balance|       city|               email| employer|firstname|gender| lastname|state|
+     * +--------------+--------------------+---+-------+-----------+--------------------+---------+---------+------+---------+-----+
+     * |             1|     880 Holmes Lane| 32|  39225|     Brogan|amberduke@pyrami.com|   Pyrami|    Amber|     M|     Duke|   MD|
+     * |             6|  671 Bristol Street| 36|   5686|      Dante|hattiebond@netagy...|   Netagy|   Hattie|     M|     Bond|   MD|
+     * |            13|  789 Madison Street| 28|  32838|      Nogal|nanettebates@quil...|  Quility|  Nanette|     F|    Bates|   VA|
+     * |            18|467 Hutchinson Court| 33|   4180|      Orick| daleadams@boink.com|    Boink|     Dale|     M|    Adams|   MD|
+     * |            20|     282 Kings Place| 36|  16418|     Ribera|elinorratliff@sce...| Scentric|   Elinor|     M|  Ratliff|   MD|
+     * |            25|   171 Putnam Avenue| 39|  40540|  Nicholson|virginiaayala@fil...| Filodyne| Virginia|     F|    Ayala|   VA|
+     * |            32|  702 Quentin Street| 34|  48086|    Veguita|dillardmcpherson@...| Quailcom|  Dillard|     F|Mcpherson|   VA|
+     * |            37|  826 Fillmore Place| 39|  18612| Tooleville|mcgeemooney@rever...| Reversus|    Mcgee|     M|   Mooney|   VA|
+     * |            44|502 Baycliff Terrace| 37|  34487|  Yardville|aureliaharding@or...|  Orbalix|  Aurelia|     M|  Harding|   VA|
+     * |            49| 451 Humboldt Street| 23|  29104|   Sunriver|fultonholt@anocha...|   Anocha|   Fulton|     F|     Holt|   RI|
+     * |            51|    334 River Street| 31|  14097|Jacksonburg|burtonmeyers@beza...|    Bezal|   Burton|     F|   Meyers|   RI|
+     * |            56|     857 Tabor Court| 32|  14992|  Sunnyside|josienelson@emtra...|   Emtrac|    Josie|     M|   Nelson|   RI|
+     * |            63| 510 Sedgwick Street| 30|   6077|   Guilford|hughesowens@valpr...| Valpreal|   Hughes|     F|    Owens|   KS|
+     * |            68|     927 Bay Parkway| 25|  44214|    Shawmut| hallkey@eventex.com|  Eventex|     Hall|     F|      Key|   KS|
+     * |            70|     685 School Lane| 33|  38172|   Chestnut|deidrethompson@ne...| Netplode|   Deidre|     F| Thompson|   RI|
+     * |            75| 166 Irvington Place| 22|  40500|  Limestone|sandovalkramer@ov...| Overfork| Sandoval|     F|   Kramer|   RI|
+     * |            82|   195 Bayview Place| 39|  41412|Summerfield|concettabarnes@fi...|  Fitcore| Concetta|     F|   Barnes|   KS|
+     * |            87|  446 Halleck Street| 22|   1133|   Coalmont|hewittkidd@isolog...|Isologics|   Hewitt|     M|     Kidd|   KS|
+     * |            94|  183 Kathleen Court| 30|  41060| Cornucopia|brittanycabrera@m...|   Mixers| Brittany|     F|  Cabrera|   KS|
+     * |            99|  806 Rockwell Place| 39|  47159|      Shaft|ratliffheath@zapp...|   Zappix|  Ratliff|     F|    Heath|   KS|
+     * +--------------+--------------------+---+-------+-----------+--------------------+---------+---------+------+---------+-----+
+     */
+  }
+
 
 }
