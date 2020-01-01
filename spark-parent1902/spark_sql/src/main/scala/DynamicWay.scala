e1f4674c91114fd80be9ba3ca18a846aa8d9555d
aleam0722 <cxb444555@outlook.com>
2020-01-01 16:11:09 +0800

    对于采用动态编码方式创建dataFrame的一些补充说明

--- spark-parent1902/spark_sql/src/main/scala/DynamicWay.scala
+++ spark-parent1902/spark_sql/src/main/scala/DynamicWay.scala
@@ -8,6 +8,8 @@ object DynamicWay {
     dynamicWay(spark)
   }
   def dynamicWay(spark: SparkSession):Unit = {
+
+    /*这里是行，也就是我们传递一条一条的数据，使用row对象进行封装*/
     val row = spark.sparkContext.parallelize(
       List(
           Row(1, "李伟", 1, 180.0),
@@ -16,7 +18,10 @@ object DynamicWay {
           Row(4, "麻宁娜", 0, 168.0)
       )
     )
-
+/*这里是表头信息，也就是我们每行每个数据所对应的的字段信息，这里采用StrutType对象进行封装，这个StrutType对象的构造器
+  要求我们传递StructFile（结构化字段）的对象参数集合作为数据载体，结合上一步骤的行，这一步骤的列我们能够构建出具有表头信息和每行数据
+  的完成的表格 （dataFrame）
+*/
     val schema = types.StructType(
       List(
           StructField("id", DataTypes.IntegerType, false),
@@ -25,6 +30,7 @@ object DynamicWay {
           StructField("height", DataTypes.DoubleType, false)
       )
     )
+    /*createDataFrame其中的一个构造器中允许传递RDD和StrutType对象创建DataFrame，也就是需要完整的行列信息才能创建表*/
     val dataFrame = spark.createDataFrame(row,schema).show()
     spark.stop()
   }
